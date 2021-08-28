cf906e76abdff72cd296fcfdf10b74d74b426201
William Watson <williamcwatson@protonmail.com>
2021-08-28 17:11:51 -0400

    Midway through the data types thing, wanted to commit real fast

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,45 @@
-let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN!");
+// let js = "amazing";
 
-let math = 40+29+14-39
-console.log(math)
+// let math = 40+29+14-39
+// console.log(math)
+
+// let firstName = "Jonas";
+// console.log(firstName);
+
+// // variables work basically how I'd expect, use let to create them
+// // this tutorial uses camel case variable names, so there's that
+// // can't start variables with numbers, you'll get a syntax error
+// // only a-Z, 0-9, _, and $ allowed in variabes.
+// // don't use reserve keywords for variable names, even if you can.
+// // capital first letter names are for objects and classes and stuff.
+// // all caps used for constants
+// // better to use descriptive variable names, e.g. not using variableName0 and variableName1
+
+// //Data Types
+
+// // all values are either objects or primitives
+// // Number, basically floats and doubles, also used for ints
+// // String, sequence of characters
+// // boolean, true/false
+// // undefined, for variables that have not been assigned a value
+// // Null, an empty value
+// // symbol, value that is unique and cannot be changed
+// //      seems very useful for functional stuff
+// // BigInt, another type for numbers, for really big ints
+
+// // JS uses dynamic typing, such that only values have types, not variables.
+// // variables can start as numbers and be assigned to strings at a later time.
+// // could lead to difficult-to-find bugs
+
+/*  
+    multiline 
+    comments
+*/
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof javaScriptIsFun);
+
+
+
